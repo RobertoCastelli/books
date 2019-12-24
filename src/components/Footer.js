76c9e11c6e802642535951301f0da7085023a98e
RobertoCastelli <roberto.castelli.teal@gmail.com>
2019-12-24 22:38:45 +0100

    First commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer>
-            copyright &copy; 2019 Roberto Castelli www..robertocastelli.dev
+            copyright &copy; 2019 Roberto Castelli www.robertocastelli.dev
         </footer>
     )
 }
